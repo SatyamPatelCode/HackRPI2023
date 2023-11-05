1a42d775412838d343b6fa57e13cd2a34a70ea15
Emmanuelusman2004 <117528761+Emmanuelusman2004@users.noreply.github.com>
2023-11-04 22:33:37 -0400

    made h1 link to jhome[page

---
+++ eventhandler.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the H1 element by its ID
+    var title = document.getElementById("myHeader");
+    
+    // Add a click event listener to the H1 element
+    title.addEventListener("click", function() {
+        // Redirect to the homepage (change the URL to your actual homepage)
+        window.location.href = "index.html";
+    });
+});
